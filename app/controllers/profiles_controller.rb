815a36496aa2dcec830e3d4910f547450df0215d
AmiasYaska <amiasyaska@gmail.com>
2025-02-07 14:24:07 +0300

    changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,22 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
   def edit
+    @profile = current_user
   end
+
+  def update
+    @profile = current_user
+    if @profile.update(set_params)
+      redirect_to profile_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def set_params
+    params.expect(user: [:username, :email])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "profiles/edit"
-  get "pages/index"
+  resource :profile, only: [:edit, :update]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
