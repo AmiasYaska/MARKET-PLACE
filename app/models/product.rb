79a4c15752ac23d1f5fa5031e186e22c5ee6986e
AmiasYaska <amiasyaska@gmail.com>
2025-02-08 15:41:05 +0300

    changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_many_attached :images
+
+  belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   validates :username, presence: true
 
+  has_many :products
 end
