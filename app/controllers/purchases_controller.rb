3f4994003dc1c51a266f622bfe9ca9e5e87f89e9
AmiasYaska <amiasyaska@gmail.com>
2025-02-09 14:05:18 +0300

    changes

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,19 +1,20 @@
-class PurchasesController < Applicationcontroller
+class PurchasesController < ApplicationController
     before_action :authenticate_user!
-
+  
     def create
-        @product = Product.find(params[:product_id])
-
-        if @product.user = current_user
-            redirect_to products_path, alert: "You created this product"
-            return
-        end
-
-        @purchase = current_user.purchases.create(product: @product)
-        if @purchase.persisted?
-            redirect_to products_path, notice: "Purchase successful"
-        else
-            redirect_to products_path, alert: "Purchase failed. Try again" 
-        end
+      @product = Product.find(params[:product_id])
+      
+      if @product.user == current_user
+        redirect_to products_path, alert: "You cannot purchase your own product."
+        return
+      end
+  
+      @purchase = current_user.purchases.create(product: @product)
+      
+      if @purchase.persisted?
+        redirect_to products_path, notice: "Purchase successful!"
+      else
+        redirect_to products_path, alert: "Purchase failed."
+      end
     end
-end
+  end
